c51a669a214ee37e6088357948284c269198e725
KostromDan <kostromdan@gmail.com>
2025-05-22 03:24:27 +0300

    Small fixes.

---
+++ src/main/java/dev/kostromdan/mods/crash_assistant/vulkan_addon/VulkanAddon.java
@@ -0,0 +1,13 @@
+package dev.kostromdan.mods.crash_assistant.vulkan_addon;
+
+/**
+ * Vulkan addon for Crash Assistant
+ */
+public class VulkanAddon {
+    
+    public VulkanAddon() {
+        // Constructor
+    }
+    
+    // Add methods as needed
+}
