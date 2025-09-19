19a5e1e96682f3331482d0d896676925313a0445
KostromDan <kostromdan@gmail.com>
2025-09-19 12:42:06 +0300

    Add conditional support for Technic launcher logs detection

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/CrashAssistantApp.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/CrashAssistantApp.java
@@ -273,9 +273,11 @@ public class CrashAssistantApp {
         LogsList.addIfExistsAndModified(new Log(LogType.LAUNCHER_LOG, Paths.get("../../../logs", "PollyMC-0.log")));
 
         LogsList.addIfExistsAndModified(new Log(LogType.LAUNCHER_LOG, "Feather: latest.log", Paths.get("feather/logs", "latest.log")));
-        FileUtils.getModifiedFiles(Paths.get("../../../.technic/logs"), ".log").forEach(path -> {
-            LogsList.addIfExistsAndModified(new Log(LogType.LAUNCHER_LOG, path));
-        });
+        if (Paths.get("../../").toAbsolutePath().normalize().getFileName().toString().contains("technic")) {
+            FileUtils.getModifiedFiles(Paths.get("../../logs"), ".log").forEach(path -> {
+                LogsList.addIfExistsAndModified(new Log(LogType.LAUNCHER_LOG, path));
+            });
+        }
 
         String userHome = System.getProperty("user.home");
         if (userHome != null) {
