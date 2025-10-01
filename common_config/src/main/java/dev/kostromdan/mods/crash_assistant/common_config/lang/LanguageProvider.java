25907eaaf68c5cb1954296b65dcfc240909f8206
KostromDan <kostromdan@gmail.com>
2025-10-01 04:20:56 +0300

    Fix lang.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/lang/LanguageProvider.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/lang/LanguageProvider.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 public class LanguageProvider {
     private static final Logger LOGGER = LogManager.getLogger();
     public static Path OPTIONS_PATH = Paths.get("options.txt");
-    public static Path LANG_PATH = Paths.get("config", "crash_assistant", "lang");
+    public static Path LANG_PATH = Paths.get("config", "crash_assistant", "ca_lang");
     public static HashMap<String, Lang> languages = new HashMap<>();
     public static String currentLangName;
     public static String msgLangName;
@@ -71,13 +71,13 @@ public class LanguageProvider {
         CrashAssistantConfig.executeWithLock(() -> {
             LANG_PATH.toFile().mkdirs();
             HashSet<String> langFilesInJarNames = new HashSet<>();
-            langFilesInJarNames.add("lang/de_de.json");
-            langFilesInJarNames.add("lang/en_us.json");
-            langFilesInJarNames.add("lang/ru_ru.json");
-            langFilesInJarNames.add("lang/zh_cn.json");
-            langFilesInJarNames.add("lang/es_es.json");
-            langFilesInJarNames.add("lang/it_it.json");
-            langFilesInJarNames.add("lang/README.md");
+            langFilesInJarNames.add("ca_lang/de_de.json");
+            langFilesInJarNames.add("ca_lang/en_us.json");
+            langFilesInJarNames.add("ca_lang/ru_ru.json");
+            langFilesInJarNames.add("ca_lang/zh_cn.json");
+            langFilesInJarNames.add("ca_lang/es_es.json");
+            langFilesInJarNames.add("ca_lang/it_it.json");
+            langFilesInJarNames.add("ca_lang/README.md");
             HashMap<String, HashMap<String, String>> jarLangFiles = new HashMap<>();
             for (String langFile : langFilesInJarNames) {
                 if (langFile.endsWith("/")) {
